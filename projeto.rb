8e97b504f9320e6d79858160c23cd95a4d433580
Grasiela Texeira <grasy.mst@gmail.com>
2023-11-08 16:00:00 -0300

    Projeto Ruby Dio

---
+++ projeto.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome:  "
+nome = gets.chomp
+
+print "Digite seu sobrenome:  "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.to_i
+
+puts "Meu nome é #{nome} #{sobrenome} e tenho #{idade} anos."
